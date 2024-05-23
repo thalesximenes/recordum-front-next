1e4131d68df059d52bb562852034da04aaf77c82
Thales Fued Ximenes Ferreira <99039988@sop.local>
2024-05-23 18:29:42 -0300

    refactor: adding svg support

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,3 +1,4 @@
+const path = require("path");
 export default {
   stories: ["../src/**/stories.mdx", "../src/**/stories.@(js|jsx|ts|tsx)"],
   framework: {
@@ -7,5 +8,30 @@ export default {
         useSWC: true,
       },
     },
+  },
+  webpackFinal: async (config) => {
+    const fileLoaderRule = config.module.rules.find(
+      (rule) => rule.test && rule.test.test(".svg")
+    );
+    fileLoaderRule.exclude = /\.svg$/;
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "../src/"),
+    };
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      process: require.resolve("process/browser"),
+      zlib: require.resolve("browserify-zlib"),
+      stream: require.resolve("stream-browserify"),
+      util: require.resolve("util"),
+      buffer: require.resolve("buffer"),
+      asset: require.resolve("assert"),
+    };
+    config.module.rules.push({
+      test: /\.svg$/,
+      enforce: "pre",
+      loader: require.resolve("@svgr/webpack"),
+    });
+    return config;
   },
 };
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,35 @@
+const scope = "/recordum";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: scope,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [{
+        loader: '@svgr/webpack',
+        options: {
+          svgo: true,
+          svgoConfig: {
+            plugins: [{
+              name: 'preset-default',
+              params: {
+                overrides: {
+                  removeViewBox: false,
+                },
+              },
+            },
+              'removeDimensions',
+            ],
+          },
+        },
+      },],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
