89a8b1ed2c382b2cf43948d471ffe9bc95054441
Thales Fued Ximenes Ferreira <99039988@sop.local>
2024-10-08 16:16:56 -0300

    feat: transform application into a PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,35 +1,51 @@
+import customCache from './public/customCache.mjs';
+import withPWA from 'next-pwa';
+
 const scope = "/recordum";
+const configWithPWA = withPWA({
+    dest: 'public',
+    fallbacks: {
+        document: `${scope}/500`,
+    },
+    runtimeCaching: customCache,
+});
+
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  basePath: scope,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: [{
-        loader: '@svgr/webpack',
-        options: {
-          svgo: true,
-          svgoConfig: {
-            plugins: [{
-              name: 'preset-default',
-              params: {
-                overrides: {
-                  removeViewBox: false,
+    reactStrictMode: false,
+    basePath: scope,
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: [{
+                loader: '@svgr/webpack',
+                options: {
+                    svgo: true,
+                    svgoConfig: {
+                        plugins: [{
+                                name: 'preset-default',
+                                params: {
+                                    overrides: {
+                                        removeViewBox: false,
+                                    },
+                                },
+                            },
+                            'removeDimensions',
+                        ],
+                    },
                 },
-              },
-            },
-              'removeDimensions',
-            ],
-          },
-        },
-      },],
-    });
+            }, ],
+        });
 
-    return config;
-  },
+        return config;
+    },
 };
 
-export default nextConfig;
+const pwaNextConfig = () => {
+    const plugins = [configWithPWA];
+    return plugins.reduce((acc, next) => next(acc), nextConfig);
+};
+
+export default pwaNextConfig;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,18 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href={`/recordum/favicon.ico`} />
+        <link rel="manifest" href={`/recordum/manifest.json`} />
+        <meta name="theme-color" content="#000000" />
+        <link
+          rel="apple-touch-icon"
+          href={`/recordum/icons/icon-192x192.png`}
+        />
+        <meta name="description" content="Recordum" />
+      </Head>
       <body>
         <Main />
         <NextScript />
